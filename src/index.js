15f43b4f362a79eba7c2e3e5d85474a565464828
rogelio-brill <99710794+rogelio-brill@users.noreply.github.com>
2022-05-27 12:15:33 -0700

    completed ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import Tooltip from './ui/tooltip';
+import Dropdown from './ui/dropdown';
+import Tabs from './ui/tabs';
+import Snackbar from './ui/snackbar';
 
 // Create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
 tooltip.init();
+
+// Create dropdowns
+const dropdowns = document.querySelector('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+});
+
+// Create Tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+// Create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('You clicked me!');
+});
